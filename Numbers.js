5d95c46ee57a901378c35807dc1c8ec4821cec7a
Eugene-Kwaka <kwakaeugene@gmail.com>
2022-10-11 17:05:28 -0500

    Worked on Numbers in JS

---
+++ Numbers.js
@@ -0,0 +1,12 @@
+// Arithmentic operator
+console.log(5 + 20);
+
+// Multiple Operators
+console.log(5 * 3 + 5 - 12);
+
+// Modulo operator
+console.log(14 % 5);
+
+// Order of operators
+// Modulo has a higher precedence over addition and multiplication
+console.log(5 + 10 - 12 % 5);
