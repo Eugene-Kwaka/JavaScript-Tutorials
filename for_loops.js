819d17d19449f53363a427b022f31948704a7c18
Eugene-Kwaka <kwakaeugene@gmail.com>
2022-11-30 14:55:33 -0600

    Learnt about for loops in JS

---
+++ for_loops.js
@@ -0,0 +1,12 @@
+// Counter reads from 1 to 3 then displays the word Go!!!!
+for (var i = 1; i <=3; i++){
+    console.log(i);
+}
+console.log("Go!!!")
+
+
+// Counting backwards from 10-1 then display Happy New Year!!!
+for (var i=10; i > 0; i--){
+    console.log(i);
+}
+console.log("Happy New Year!!!")
