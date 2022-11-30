971fa9232dd4259ce01e8e91b7c73990721169d6
Eugene-Kwaka <kwakaeugene@gmail.com>
2022-11-30 15:34:19 -0600

    Learnt about while loops in Js

---
+++ while_loops.js
@@ -0,0 +1,16 @@
+
+// Check time_count condition if its true throughout the loop and execute.
+var time_count = 10;
+while (time_count > 0) {
+    console.log(time_count);
+    time_count = time_count - 1;
+}
+console.log("Let's Go");
+
+// Use while loop to display years incrementing from 2018 to 2022
+var year = 2018;
+while (year <= 2022){
+    console.log(year);
+    year++;
+}
+console.log("It has been almost 5 years now");
