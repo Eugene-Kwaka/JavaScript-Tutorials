7f5e5f3e296d8f63ec83fc9268647ae82aef6228
Eugene-Kwaka <kwakaeugene@gmail.com>
2022-12-20 17:49:00 -0600

    Learned of null, undefined and emptyvalues/strings

---
+++ undefined_null_emptyValues.js
@@ -0,0 +1,10 @@
+// NULL datatype -> Refers to an intentional absence of object values
+// for example:
+var letters = "abc";
+console.log(letters.match(/d/)); // This will return null as the letter d is absent in the variable letters
+
+
+// UNDEFINED -> This datatype on holds one type of value which is undefined.
+// for example:
+var sports;
+console.log(sports) // This will return undefined as the console.log() function has variable that is unassigned
