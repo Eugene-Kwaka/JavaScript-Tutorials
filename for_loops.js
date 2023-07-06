1415dc157a069b030fa79977933d7bb8efefc973
Eugene-Kwaka <kwakaeugene@gmail.com>
2023-07-06 16:31:52 -0500

    More on for loops in JS

--- for_loops.js
+++ for_loops.js
@@ -1,12 +1,20 @@
 // Counter reads from 1 to 3 then displays the word Go!!!!
-for (var i = 1; i <=3; i++){
-    console.log(i);
-}
-console.log("Go!!!")
+// for (var i = 1; i <=3; i++){
+//     console.log(i);
+// }
+// console.log("Go!!!")
 
 
-// Counting backwards from 10-1 then display Happy New Year!!!
-for (var i=10; i > 0; i--){
-    console.log(i);
+// // Counting backwards from 10-1 then display Happy New Year!!!
+// for (var i=10; i > 0; i--){
+//     console.log(i);
+// }
+// console.log("Happy New Year!!!")
+
+// Sum of items in a for loop
+let nums = [1,2,3,4,5]
+let sum = 0
+for(let num = 0; num < nums.length; num++){
+    sum += nums[num]
 }
-console.log("Happy New Year!!!")
+console.log(sum)
